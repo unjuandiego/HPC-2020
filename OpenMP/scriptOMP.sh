8dc9225410643a58d11e37ba6f120da54108246d
unjuandiego <juandiegoguepe@gmail.com>
2020-04-15 21:17:35 -0500

    Update scriptOMP.sh

--- OpenMP/scriptOMP.sh
+++ OpenMP/scriptOMP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Compilando archivo fuente"
 gcc -o ompsec2 -fopenmp secuencialOMP.c 
-for i in 10 100 200 500 800 
+for i in 10 100 200 500 800 1000 2000
 do
-for j in 1 2 3 4 5 
+for j in 1 2 3 4 5 6 7 8 9 10
 do
 echo Ejecutando con matrices de $i X $i
 ./ompsec2 $i 
